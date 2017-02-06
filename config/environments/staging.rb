9fb41df66e9e229b20f3a400368f06fb193406d4
mark172 <mark172@me.com>
2017-02-06 15:37:56 -0800

    mailgun domain

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -86,6 +86,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     port: 587,
     address: "smtp.mailgun.org",
+    domain: 'macfontanaandbigd.com'
     user_name: ENV['username'],
     password: ENV['password'],
     :authentication => :plain
