a2a3968b1d79af932d87d2681409cf9202fec579
mark172 <mark172@me.com>
2017-02-06 11:10:12 -0800

    fixed syntax error in stmp settings

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -86,7 +86,6 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
     :address        => "smtp.mailgun.org",
-    :domain         => ENV['domain'],
     :user_name      => ENV['username'],
     :password       => ENV['password'],
     :authentication => :plain,
